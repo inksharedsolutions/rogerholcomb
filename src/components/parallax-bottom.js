01b23909634c3f39703f02df79b77582cf923e7b
mberonga <mberonga.stratton@gmail.com>
2021-02-23 18:52:12 +0800

    added testimonials

--- src/components/parallax-bottom.js
+++ src/components/parallax-bottom.js
@@ -14,6 +14,16 @@ import React from 'react'
                     </h1>
                 </div>
             </section>
+            <section className="testimonial-container">
+                <div className="container testimonial">
+                    <h1>&nbsp;</h1>
+                    <p>
+                        I enjoyed hearing the experiences of helping others become closer to God and the lessons the author learned along the way. The author encourages us not to take anyone for granted, but to take opportunities to grow in our relationships with others through conversation. It is through conversation that we can help bring others to God and even become closer to God ourselves. The book is a testament of how our lives can be filled with God’s grace and a good reminder that caring for others along the way is part of our responsibility as God’s children.
+                        
+                        <span className="italic-tag span-verse">&nbsp;</span>
+                    </p>
+                </div>
+            </section>
         </>
      )
  }
